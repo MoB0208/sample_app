4e10f1c54c9d172304a3ac1242678289397f8a8a
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-10-13 11:04:26 +0000

    [Finish] 1-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,11 +6,13 @@ class ListsController < ApplicationController
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-     # 4. トップ画面へリダイレクト
-     redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+
   end
 
 
@@ -49,4 +51,7 @@ end
 
 class List < ApplicationRecord
   has_one_attached :image
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
 end
